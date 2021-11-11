e2b72f0e254191c580531e94a8b69957bd872de7
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2021-11-11 20:15:40 +0300

    URLconfig

--- Horoscope/horoscope_app/views.py
+++ Horoscope/horoscope_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def leo(request):
-    return HttpResponse("Знак зодиака лев")
+    return HttpResponse("Знак зодиака Лев")
+
+
+def scorpio(request):
+    return HttpResponse("Знак зодиака Скорпион")
