56a91acc629d8af51c77431bd958562954e1dd94
ZeroCreator <59481361+ZeroCreator@users.noreply.github.com>
2021-11-11 21:18:04 +0300

    zodiac_dict

--- Horoscope/horoscope_app/urls.py
+++ Horoscope/horoscope_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('leo', views.leo),
-    path('scorpio', views.scorpio),
+    path('<sign_zodiac>', views.get_info_about_sign_zodiac),
+
 ]
